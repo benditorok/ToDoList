6766cec6f1f4ce0776905e4c8662b1d47e9051be
Török Bendegúz <32034793+benditorok@users.noreply.github.com>
2024-01-19 11:09:52 +0100

    Add ToDoItem

---
+++ src/ToDoList.Domain/Entities/ToDoItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain.Entities;
+
+public class ToDoItem : IEquatable<ToDoItem>
+{
+    public int Id { get; set; }
+
+    public string Creator { get; set; } = string.Empty;
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Body { get; set; } = string.Empty;
+
+    public bool IsDone { get; set; } = false;
+
+    public int ToDoListId { get; set; }
+
+    public virtual ToDoList? ToDoList { get; set; }
+
+    public ToDoItem()
+    {
+    }
+
+    public ToDoItem(int id, string creator, string title, string body, bool isDone, int toDoListId)
+    {
+        Id = id;
+        Creator = creator;
+        Title = title;
+        Body = body;
+        IsDone = isDone;
+        ToDoListId = toDoListId;
+    }
+
+    public bool Equals(ToDoItem? other)
+    {
+        return GetHashCode() == other?.GetHashCode();
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Creator, Title, Body, IsDone);
+    }
+}
