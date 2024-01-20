8e302beb0d12c60d7c2e8e9c21f4e3ca09cc3f2f
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-01-20 20:18:30 +0100

    Updated entities

---
+++ src/ToDoList.Domain/Entities/Note.cs
@@ -0,0 +1,32 @@
+﻿namespace ToDoList.Domain.Entities;
+
+public class Note : IEquatable<Note>
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public string? Body { get; set; }
+
+    public bool IsDone { get; set; } = false;
+
+    public DateTime? Reminder { get; set; }
+
+    public int NoteListId { get; set; }
+
+    public virtual NoteList? NoteList { get; set; }
+
+    public Note()
+    {
+    }
+
+    public bool Equals(Note? other)
+    {
+        return GetHashCode() == other?.GetHashCode();
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Title, Body, IsDone, Reminder, NoteListId);
+    }
+}
---
+++ src/ToDoList.Domain/Entities/NoteList.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace ToDoList.Domain.Entities;
+
+public class NoteList : IEquatable<NoteList>
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public Color Color { get; set; } = Color.White;
+
+    public virtual ICollection<Note> Notes { get; set; } = new HashSet<Note>();
+
+    public NoteList()
+    {
+    }
+
+    public bool Equals(NoteList? other)
+    {
+        return GetHashCode() == other?.GetHashCode();
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Title, Color, Notes);
+    }
+}
