4e85b0876891216c4e11825846a8c7a66bc145c9
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-01-20 22:12:17 +0100

    Update dbcontext

---
+++ src/ToDoList.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Infrastructure.Database;
+
+internal class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+{
+    public virtual DbSet<Note> Notes => Set<Note>();
+
+    public virtual DbSet<NoteList> NoteLists => Set<NoteList>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        #region Note entity
+
+        // Primary key
+        modelBuilder.Entity<Note>()
+            .HasKey(x => x.Id);
+
+        // Auto increment
+        modelBuilder.Entity<Note>()
+            .Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        #endregion Note entity
+
+        #region NoteList entity
+
+        // Primary key
+        modelBuilder.Entity<NoteList>()
+            .HasKey(x => x.Id);
+
+        // Auto increment
+        modelBuilder.Entity<NoteList>()
+            .Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        // Connection between Note and NoteList
+        modelBuilder.Entity<NoteList>()
+            .HasMany(x => x.Notes)
+            .WithOne(x => x.NoteList)
+            .HasForeignKey(x => x.NoteListId);
+
+        #endregion NoteList entity
+
+        #region Identity
+
+        // TODO add roles, seed
+
+        #endregion Identity
+    }
+}
