31d7f8fcc6657e485707141b70f1c6287c105c56
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-01-20 22:13:25 +0100

    Added basic logic implementations

---
+++ src/ToDoList.Application/Logic/NoteListLogic.cs
@@ -0,0 +1,35 @@
+﻿using ToDoList.Application.Interfaces.Repository;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Application.Logic;
+
+public class NoteListLogic
+{
+    private IRepository<NoteList> _repo;
+
+    public NoteListLogic(IRepository<NoteList> repo)
+    {
+        _repo = repo;
+    }
+
+    // TODO Implement logic
+    public async Task CreateAsync(NoteList item)
+    {
+        await _repo.CreateAsync(item);
+    }
+
+    public async Task<NoteList> ReadAsync(int id)
+    {
+        return await _repo.ReadAsync(id);
+    }
+
+    public async Task UpdateAsync(NoteList item)
+    {
+        await _repo.UpdateAsync(item);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await _repo.DeleteAsync(id);
+    }
+}
---
+++ src/ToDoList.Application/Logic/NoteLogic.cs
@@ -0,0 +1,35 @@
+﻿using ToDoList.Application.Interfaces.Repository;
+using ToDoList.Domain.Entities;
+
+namespace ToDoList.Application.Logic;
+
+public class NoteLogic
+{
+    private IRepository<Note> _repo;
+
+    public NoteLogic(IRepository<Note> repo)
+    {
+        _repo = repo;
+    }
+
+    // TODO Implement logic
+    public async Task CreateAsync(Note item)
+    {
+        await _repo.CreateAsync(item);
+    }
+
+    public async Task<Note> ReadAsync(int id)
+    {
+        return await _repo.ReadAsync(id);
+    }
+
+    public async Task UpdateAsync(Note item)
+    {
+        await _repo.UpdateAsync(item);
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await _repo.DeleteAsync(id);
+    }
+}
