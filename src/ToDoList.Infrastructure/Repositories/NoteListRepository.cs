2dfc13f2e5aba76f0f3b9124221161f63ac39630
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-01-20 22:11:31 +0100

    Add repository implementations

---
+++ src/ToDoList.Infrastructure/Repositories/NoteListRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using ToDoList.Domain.Entities;
+using ToDoList.Infrastructure.Database;
+
+namespace ToDoList.Infrastructure.Repositories;
+
+internal class NoteListRepository : Repository<NoteList>
+{
+    public NoteListRepository(ApplicationDbContext ctx) : base(ctx)
+    {
+    }
+
+    public override async Task<NoteList> ReadAsync(int id)
+    {
+        return await _ctx.NoteLists.FirstOrDefaultAsync(x => x.Id == id) ?? null!;
+    }
+
+    public override async Task UpdateAsync(NoteList item)
+    {
+        var old = await ReadAsync(item.Id);
+
+        foreach (PropertyInfo prop in old.GetType().GetProperties())
+        {
+            if (prop.GetAccessors().FirstOrDefault(x => x.IsVirtual) == null)
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+        }
+
+        await _ctx.SaveChangesAsync();
+    }
+}
---
+++ src/ToDoList.Infrastructure/Repositories/NoteRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using ToDoList.Domain.Entities;
+using ToDoList.Infrastructure.Database;
+
+namespace ToDoList.Infrastructure.Repositories;
+
+internal class NoteRepository : Repository<Note>
+{
+    public NoteRepository(ApplicationDbContext ctx) : base(ctx)
+    {
+    }
+
+    public override async Task<Note> ReadAsync(int id)
+    {
+        return await _ctx.Notes.FirstOrDefaultAsync(x => x.Id == id) ?? null!;
+    }
+
+    public override async Task UpdateAsync(Note item)
+    {
+        var old = await ReadAsync(item.Id);
+
+        foreach (PropertyInfo prop in old.GetType().GetProperties())
+        {
+            if (prop.GetAccessors().FirstOrDefault(x => x.IsVirtual) == null)
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+        }
+
+        await _ctx.SaveChangesAsync();
+    }
+}
