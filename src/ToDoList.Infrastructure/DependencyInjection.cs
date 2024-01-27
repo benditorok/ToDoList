8a219c864a54f98e5541d64d636b6798b54f45d4
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-01-27 20:25:01 +0100

    Update project to use postgres if available

--- src/ToDoList.Infrastructure/DependencyInjection.cs
+++ src/ToDoList.Infrastructure/DependencyInjection.cs
@@ -3,7 +3,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ToDoList.Application.Common.Interfaces;
 using ToDoList.Domain.Entities;
+using ToDoList.Infrastructure.Data;
 using ToDoList.Infrastructure.Database;
+using ToDoList.Infrastructure.Identity;
 using ToDoList.Infrastructure.Repositories;
 
 namespace ToDoList.Infrastructure;
@@ -12,13 +14,31 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
+        // Configuring the database
         services.AddDbContext<ApplicationDbContext>((optionsBuilder) =>
         {
-            optionsBuilder
-                .UseInMemoryDatabase("todolist")
-                .UseLazyLoadingProxies();
+            string? postgreConnStr = Environment.GetEnvironmentVariable("POSTGRE_TODOLIST") ?? 
+                Environment.GetEnvironmentVariable("POSTGRE_TODOLIST", EnvironmentVariableTarget.User);
+
+            if (!string.IsNullOrWhiteSpace(postgreConnStr))
+            {
+                optionsBuilder
+                    .UseNpgsql(postgreConnStr, x => x.MigrationsAssembly("ToDoList.Infrastructure"))
+                    .UseLazyLoadingProxies();
+            }
+            else 
+            {
+                optionsBuilder
+                    .UseInMemoryDatabase("todolist")
+                    .UseLazyLoadingProxies();
+            }
         });
 
+        services.AddScoped<ApplicationDbContextInitializer>();
+
+        services.AddSingleton(TimeProvider.System);
+
+        // Repositories
         services.AddScoped<IRepository<Note>, NoteRepository>();
         services.AddScoped<IRepository<NoteList>, NoteListRepository>();
 
