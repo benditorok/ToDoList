ba40182f84b0aa1cbecab6fa7c893f96a5c65d3a
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-01-29 16:39:32 +0100

    Add entity constants

---
+++ src/ToDoList.Domain/Constants/NoteConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace ToDoList.Domain.Constants;
+
+public class NoteConstants
+{
+    public static readonly int TilteMinLength = 1;
+
+    public static readonly int TilteMaxLength = 50;
+
+    public static readonly int BodyMinLength = 0;
+
+    public static readonly int BodyMaxLength = 500;
+}
---
+++ src/ToDoList.Domain/Constants/NoteListConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Domain.Constants;
+
+public class NoteListConstants
+{
+    public static readonly int TilteMinLength = 1;
+
+    public static readonly int TilteMaxLength = 50;
+
+    public static readonly int ColorRGBALength = 9;
+}
