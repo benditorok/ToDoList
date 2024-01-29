f062c47070221aa210a9a04f9ff4049ec3a46b77
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-01-29 13:12:50 +0100

    Add converter

---
+++ src/ToDoList.Client/Common/CompletionStateToColorConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace ToDoList.Client.Common;
+
+public class CompletionStateToColorConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool isDone)
+        {
+            if (isDone)
+            {
+                return Color.FromRgba("#00FF005F");
+            }
+            else
+            {
+                return Color.FromRgba("#FF00005F");
+            }
+        }
+
+        return default(Color);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
