1addd71d8e7f8f651df584a4fb852531279924f6
Bendegúz Török <32034793+benditorok@users.noreply.github.com>
2024-02-03 14:17:44 +0100

    Fix correct values

--- src/ToDoList.Domain/Constants/NoteListConstants.cs
+++ src/ToDoList.Domain/Constants/NoteListConstants.cs
@@ -12,7 +12,7 @@ public class NoteListConstants
 
     public static readonly char[] ColorRGBAChars =
     {
-        '1', '2', '3', '4', '5', '6', '7', '8', '9',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'A', 'B', 'C', 'D', 'E', 'F',
         'a', 'b', 'c', 'd', 'e', 'f',
         '#'
